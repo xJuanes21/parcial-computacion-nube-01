e7eb1143fc25f43c1b02822664f5f23579d5da6e
Oscar H. Mondragon <oscar.mondragon@gmail.com>
2026-02-20 08:26:41 -0500

    Reorganize SQL script for database setup
    Removing products and orders tables to separate databases

--- init.sql
+++ init.sql
@@ -10,22 +10,8 @@ CREATE TABLE users (
     password varchar(255)
 );
 
-CREATE TABLE products (
-    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name varchar(255),
-    price int,
-    quantity int);
-
-CREATE TABLE orders (
-    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    userName varchar(255),
-    userEmail varchar(255),
-    saleTotal decimal(10,2),
-    date datetime default current_timestamp);
-
-
 INSERT INTO users VALUES(null, "juan", "juan@gmail.com", "juan", "123"),
     (null, "maria", "maria@gmail.com", "maria", "456");
 
-INSERT INTO products VALUES(null, "pc", "150", "10"),
-    (null, "phone", "100", "20");
+
+
